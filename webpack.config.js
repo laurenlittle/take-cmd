45e7f17607927236078783273d05173958dc4880
laurenlittle <littlelauren016@gmail.com>
2020-02-24 06:42:10 -0500

    include babel and html loaders, update htmlwebpackplugin to use template for react

--- webpack.config.js
+++ webpack.config.js
@@ -6,14 +6,30 @@ module.exports = (env) => {
   return {
     entry: './src/index.js',
     mode: env.mode,
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: ['babel-loader']
+        },
+        {
+          test: /\.html$/,
+          use: ['html-loader']
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['*', '.js', '.jsx']
+    },
     output: {
       path: __dirname + '/dist',
       publicPath: '/',
       filename: 'bundle.js'
     },
-    plugins: [
-      new HtmlWebpackPlugin(), 
-      new webpack.ProgressPlugin()
-    ]
+    plugins: [new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html"
+    }), new webpack.ProgressPlugin()]
   };
 };
