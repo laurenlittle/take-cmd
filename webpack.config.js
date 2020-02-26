90206ad05cbeb81694263011cbfb685a3e584144
laurenlittle <littlelauren016@gmail.com>
2020-02-25 21:51:57 -0500

    adding rules/loaders for SCSS support, add MiniCssExtractPlugin, update HtmlWebpackPlugin to use template file for React

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const webpack = require('webpack');
+const isDevelopment = process.env.NODE_ENV === 'development';
 
 module.exports = (env) => {
   console.log(env)
@@ -16,20 +18,60 @@ module.exports = (env) => {
         {
           test: /\.html$/,
           use: ['html-loader']
+        },
+        {
+          test: /\.module\.s(a|c)ss$/, // SCSS Modules
+          loader: [
+            isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true,
+                sourceMap: isDevelopment
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: isDevelopment
+              }
+            }
+          ]
+        },
+        {
+          test: /\.s(a|c)ss$/, // SCSS
+          exclude: /\.module.(s(a|c)ss)$/,
+          loader: [
+            isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+            'css-loader',
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: isDevelopment
+              }
+            }
+          ]
         }
       ]
     },
     resolve: {
-      extensions: ['*', '.js', '.jsx']
+      extensions: ['*', '.js', '.jsx', '.scss']
     },
     output: {
       path: __dirname + '/dist',
       publicPath: '/',
       filename: 'bundle.js'
     },
-    plugins: [new HtmlWebpackPlugin({
-      template: "./src/index.html",
-      filename: "index.html"
-    }), new webpack.ProgressPlugin()]
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        filename: 'index.html'
+      }),
+      new MiniCssExtractPlugin({
+        filename: isDevelopment ? '[name].css' : '[name].[hash].css',
+        chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css'
+      }),
+      new webpack.ProgressPlugin()
+    ]
   };
 };
