17d987fde912312b9fbe1d30a976c6971d49646c
laurenlittle <littlelauren016@gmail.com>
2020-03-07 09:19:24 -0500

    updating and adding styles to DashMenu component

--- src/components/core/DashMenu.js
+++ src/components/core/DashMenu.js
@@ -2,30 +2,21 @@ import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import styles from './DashMenu.module'
 
-const isActive = (history, path) => {
-
-  if(history.location.pathname === path) {
-    return { color: '#FF0745' }
-  } else {
-    return { color: '#39423E' }
-  }
-}
-
 const DashMenu = () => {
   return (
     <nav className={styles.navbar}>
-      <div className={styles.headerLogo}>
-        <Link to='/'>Take-CMD</Link>
+      <div className={styles.logoContainer}>
+        <Link className={styles.headerLogo} to='/'><i className="far fa-file-alt"></i>Take-CMD</Link>
       </div>
       <ul className={styles.menuItems}>
-        <li className={styles.singleMenuItem}>
-          <Link to='/calendar'><i className="far fa-calendar-check"></i></Link>
+        <li>
+          <Link className={styles.singleMenuItem} to='/calendar'><i className="far fa-calendar-check"></i></Link>
         </li>
-        <li className={styles.singleMenuItem}>
-          <Link to='/profile'><i className="fas fa-user-circle"></i></Link>
+        <li>
+          <Link className={styles.singleMenuItem} to='/profile'><i className="fas fa-user-circle"></i></Link>
         </li>
-        <li className={`${styles.singleMenuItem} ${styles.menuItemBtn}`}>
-          <Link to='/signout'>Sign Out</Link>
+        <li>
+          <Link className={`${styles.singleMenuItem} ${styles.menuItemBtn}`} to='/signout'>Sign Out</Link>
         </li>
       </ul>
     </nav>
