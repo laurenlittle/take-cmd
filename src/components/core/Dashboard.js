180b5fa1910c76ee88bac449e95792173f077e43
laurenlittle <littlelauren016@gmail.com>
2020-03-26 08:29:44 -0400

    updating/including state, methods, and forms to edit, add docs to parent Dashboard

--- src/components/core/Dashboard.js
+++ src/components/core/Dashboard.js
@@ -3,22 +3,50 @@ import { firestore } from '../../firebase';
 import { addIdToDoc } from '../../utilities';
 import DashLayout from './DashLayout';
 import AddDocument from './AddDocument';
-import DocumentsList from './DocumentsList';
+import EditDocument from './EditDocument';
+import Document from './Document';
 import styles from './Dashboard.module';
 
 const Dashboard = () => {
 
   const [documentsList, setDocumentsList] = useState([]);
+  const [editing, setEditing] = useState(false);
+  const [currentDoc, setCurrentDoc] = useState();
+  const [documentRef, setDocumentRef] = useState();
 
   const loadDocuments = async () => {
-
     // Subscribe to DB changes
     firestore.collection('documents').onSnapshot(snapshot => {
       const documents = snapshot.docs.map(addIdToDoc);
       setDocumentsList(documents);
     })
-
   };
+
+  const addDocument = document => {
+    firestore.collection('documents').add(document);
+  };
+
+  const updateDocument = (currentDoc, documentRef) => {
+    setEditing(false);
+
+    return documentRef.update({
+        title: currentDoc.title,
+        description: currentDoc.description
+      })
+      .then(function () {
+        console.log("Document successfully updated!");
+      })
+      .catch(function (error) {
+        // The document probably doesn't exist.
+        console.error("Error updating document: ", error);
+      });
+  }
+
+  const editDocument = (document, dbDocRef) => {
+    setEditing(true);
+    setCurrentDoc(document);
+    setDocumentRef(dbDocRef);
+  }
 
   useEffect(() => {
     loadDocuments();
@@ -30,11 +58,26 @@ const Dashboard = () => {
     <section className={styles.dashLeft}>
       <h1>Discover Your Accomplishments</h1>
       <p>Search and Sort</p>
-      <DocumentsList docList={documentsList} />
+      {documentsList.map(document => <Document {...document} editDocument={editDocument} key={document.id} />)}
     </section>
 
     <section className={styles.dashRight}>
-     <AddDocument />
+       {editing ? (
+          <div>
+            <h2>Edit Document</h2>
+            <EditDocument
+              setEditing={setEditing}
+              documentRef={documentRef}
+              currentDoc={currentDoc}
+              updateDocument={updateDocument}
+            />
+          </div>
+        ) : (
+          <div>
+            <h2>Add Document</h2>
+            <AddDocument addDocument={addDocument} />
+          </div>
+        )}
     </section>
 
    </DashLayout>
